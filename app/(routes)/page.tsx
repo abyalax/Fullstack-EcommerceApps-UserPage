73438e1483ed1997f3d842ccb222ce9330436ed9
abyalax <abyalaxx@gmail.com>
2024-08-19 23:00:45 +0700

    feat: Banner Page and Product Detail Page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,21 +1,22 @@
 import getBanner from "@/actions/get-banner"
 import getProducts from "@/actions/get-products"
 import Banner from "@/components/banner"
+import ProductList from "@/components/product-list"
 import Container from "@/components/UI/container"
 
 export const revalidate = 0
 
 const HomePage = async () => {
-  const products = await getProducts({  isFeatured: true })
-  const banner = await getBanner("9bd6a3cd-439f-4962-b6db-108e4d211644")
+  const products = await getProducts({ isFeatured: true })
+  const banner = await getBanner("252c2c30-45ca-400d-a287-cc87909c9e26")
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Banner data={banner}/>
-      </div>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-
+        <Banner data={banner} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 gap-4">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   )
---
+++ components/UI/product-card.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { Product } from "@/types"
+import Image from "next/image"
+import IconButton from "./icon-button"
+import { Expand } from "lucide-react"
+import Currency from "./currency"
+import { useRouter } from "next/navigation"
+
+interface ProductCardProps {
+    data: Product
+}
+
+const ProductCard = ({ data }: ProductCardProps) => {
+
+    const router = useRouter()
+
+    const handleClick = () => {
+        router.push(`/product/${data.id}`)
+    }
+
+    return (
+        <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+            {/* Image and Action */}
+            <div className="aspect-square rounded-xl bg-gray-100 relative">
+                <Image
+                    alt="Image"
+                    src={data.images?.[0]?.url}
+                    className="aspect-square rounded-md object-cover"
+                    fill
+                />
+                <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+                    <div className="flex gap-x-6 justify-center">
+                        <IconButton onClick={() => null} icon={<Expand size={20} />} className="text-gray-600" />
+                    </div>
+                </div>
+            </div>
+            {/* Product Description */}
+            <div>
+                <p className="font-semibold text-lg">{data.name}</p>
+                <p className="text-sm text-gray-500">{data.category?.name}</p>
+            </div>
+            {/* Harga */}
+            <div className="flex justify-between items-center">
+                <Currency value={data?.price} />
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard
